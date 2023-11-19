d962c43e1ef0a503e9d01fca1bd5e20c54afd15d
w0dm4n <w0dm4n@hotmail.fr>
2023-11-19 05:53:45 +0100

    minipool creation && staking rpl tokens

---
+++ src/Controllers/PoolController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Keepix.PluginSystem;
+using Keepix.SmartNodePlugin.DTO.Input;
+using Keepix.SmartNodePlugin.Services;
+using Newtonsoft.Json;
+
+namespace Keepix.SmartNodePlugin.Controllers
+{
+    internal class PoolController
+    {
+        private static PluginStateManager stateManager;
+
+        [KeepixPluginFn("stake-rpl")]
+        public static async Task<bool> OnStakingRpl(StakeRplInput input)
+        {
+            stateManager = PluginStateManager.GetStateManager();
+            bool isRegistered = false;
+            bool isAlive = SetupService.IsNodeRunning();
+            bool isSync = StateService.IsNodeSynced();
+            
+            try { isRegistered = stateManager.DB.Retrieve<bool>("REGISTERED"); } catch (Exception) {}
+            if (!isRegistered) {
+                Console.WriteLine("Please first register your node on the network to create a pool.");
+                return false;
+            }
+            if (!isAlive) {
+                Console.WriteLine("Please start the node before trying to stake RPL");
+                return false;
+            }
+            if (!isSync) {
+                Console.WriteLine("Your ETH node is not synced yet on the network, please try later.");
+                return false;
+            }
+            string error = PoolService.StakeRPL(input.Amount);
+            if (!string.IsNullOrEmpty(error)) {
+                Console.WriteLine($"An error occured while trying to stake RPL: {error}");
+            }
+
+            Console.WriteLine($"You successfully staked {input.Amount} RPL.");
+            return true;
+        }
+
+        [KeepixPluginFn("create-pool")]
+        public static async Task<bool> OnPoolCreation(CreatePoolInput input)
+        {
+            stateManager = PluginStateManager.GetStateManager();
+            int poolSize = input.SmallPool ? 8 : 16;
+            bool isRegistered = false;
+            bool isAlive = SetupService.IsNodeRunning();
+            bool isSync = StateService.IsNodeSynced();
+            try { isRegistered = stateManager.DB.Retrieve<bool>("REGISTERED"); } catch (Exception) {}
+            if (!isRegistered) {
+                Console.WriteLine("Please first register your node on the network to create a pool.");
+                return false;
+            }
+            if (!isAlive) {
+                Console.WriteLine("Please start the node before trying to stake RPL");
+                return false;
+            }
+            if (!isSync) {
+                Console.WriteLine("Your ETH node is not synced yet on the network, please try later.");
+                return false;
+            }
+            var error = PoolService.CreateMiniPool(input.SmallPool);
+            if (!string.IsNullOrEmpty(error)) {
+                Console.WriteLine($"An error occured while trying to stake ETH: {error}");
+            }
+
+            Console.WriteLine($"You successfully staked {poolSize} ETH on the Ethereum network, congratulations!");
+            return true;
+        }
+    }
+}
---
+++ src/Services/PoolService.cs
@@ -0,0 +1,60 @@
+using Keepix.SmartNodePlugin.Utils;
+using System.Runtime.InteropServices;
+using Keepix.SmartNodePlugin.DTO.Input;
+using System.Xml.XPath;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+
+namespace Keepix.SmartNodePlugin.Services
+{
+    internal class PoolService
+    {
+        public static string CreateMiniPool(bool smallPool)
+        {
+            int poolSize = smallPool ? 8 : 16;
+            // smallPool true = 8ETH / smallPool false = 16 ETH
+            string result = Shell.ExecuteCommand("~/bin/rocketpool --allow-root node deposit", new List<ShellCondition>
+            {
+                new ShellCondition()
+                {
+                    content = "by creating a new minipool, your node will automatically claim",
+                    answers = new string[] {"y", ""}
+                },
+                new ShellCondition()
+                {
+                    content = "Please choose an amount of ETH to deposit",
+                    answers = smallPool ? new string[] { "1", "" } : new string[] { "2", "" }
+                }
+            });
+            if (result.Contains("not enough to create a minipool")) {
+                return $"You do not have a sufficient ETH balance on your wallet to create a {poolSize} ETH pool";
+            }
+
+            if (result.Contains("not staked enough RPL")) {
+                return $"You did not staked enough RPL to create a minipool of {poolSize} please add more first, be sure to always over-collateralize your holding.";
+            }
+            return result;
+        }
+
+        public static string StakeRPL(decimal amount) 
+        {
+            string result;
+            var cli = $"~/bin/rocketpool --allow-root node stake-rpl --amount {amount} --yes --swap";
+
+            result = Shell.ExecuteCommand(cli);
+            if (result.Contains("transfer amount exceeds balance")) {
+                return $"You do not have {amount} RPL on the node wallet, transfer them or make a swap from ETH to RPL.";
+            }
+
+            if (result.Contains("Could not get can node stake RPL status") 
+            || result.Contains("Error") || result.Contains("Could not")) 
+                return result;
+
+            if (result.Contains("Successfully staked")) {
+                return string.Empty;
+            }
+
+            return result;
+        }
+    }
+}
