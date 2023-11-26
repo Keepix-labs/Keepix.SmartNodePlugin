30a2ee5058a2f3178576e56ebd7f5236da0bccf7
jguyet <gygydu19@hotmail.fr>
2023-11-26 16:44:57 +0100

    try teku consensus

--- src/Services/SetupService.cs
+++ src/Services/SetupService.cs
@@ -126,9 +126,9 @@ namespace Keepix.SmartNodePlugin.Services
                 // var cli = $"~/bin/rocketpool --allow-root service config --smartnode-network {network} --smartnode-projectName keepix --smartnode-priorityFee 2 " + 
                 // " --executionClient nethermind --consensusClient nimbus --nimbus-additionalBnFlags '--web3-url=http://127.0.0.1:8551' --nethermind-containerTag nethermind/nethermind:latest --executionCommon-openRpcPorts external";
 
-                // besu + lighthouse
+                // besu + teku
                 var cli = $"~/bin/rocketpool --allow-root service config --smartnode-network {network} --smartnode-projectName keepix --smartnode-priorityFee 2 " + 
-                " --executionClient besu --consensusClient prysm --prysm-bnContainerTag rocketpool/prysm:latest --prysm-vcContainerTag rocketpool/prysm:latest --nethermind-containerTag hyperledger/besu:latest --executionCommon-openRpcPorts external";
+                " --executionClient besu --consensusClient teku --teku-containerTag consensys/teku:latest --nethermind-containerTag hyperledger/besu:latest --executionCommon-openRpcPorts external";
 
                 if (network == "mainnet")
                 {
